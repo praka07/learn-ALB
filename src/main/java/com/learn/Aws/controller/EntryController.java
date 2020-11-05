38ff52b2edd83903fa0d3ff0d97b3186886b3ef7
praka07 <33973861+praka07@users.noreply.github.com>
2020-11-05 18:01:33 +0530

    Update EntryController.java

--- src/main/java/com/learn/Aws/controller/EntryController.java
+++ src/main/java/com/learn/Aws/controller/EntryController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EntryController {
 
-	@Value("{msg}")
+	@Value("${msg}")
 	private String customMsg;
 
 	@GetMapping
